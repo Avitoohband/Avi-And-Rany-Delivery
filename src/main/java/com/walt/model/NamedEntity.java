d3ce1d82d1b9d405c63f98ebdeb3e3c0afa53aa3
Avi Tuchband <76391487+Avitoohband@users.noreply.github.com>
2021-10-02 20:53:36 +0300

    Add files via upload

---
+++ src/main/java/com/walt/model/NamedEntity.java
@@ -0,0 +1,57 @@
+package com.walt.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class NamedEntity {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    public NamedEntity() {
+    }
+
+    public NamedEntity(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof NamedEntity)) return false;
+
+        NamedEntity that = (NamedEntity) o;
+
+        if (!Objects.equals(id, that.id)) return false;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+}
